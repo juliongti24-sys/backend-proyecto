dfa8a95bcd881455cd018e26c9bd7c4c79a48d17
Julio Nieto <juliong.ti24@utsjr.edu.mx>
2026-03-19 18:38:56 -0600

    actualizando la dirección del frontend y el backend

--- app/main.py
+++ app/main.py
@@ -2,16 +2,20 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
-# Tus otras importaciones aquí (como la de tus rutas)
 from app.api.v1 import auth, classes, users, challenges
 
 app = FastAPI(title="API de MathBoost")
 
 # --- GUARDIA DE SEGURIDAD (CORS) ---
 # Esto le dice a FastAPI: "Permite que cualquier frontend se conecte conmigo"
+
+origins = [
+    "frontend-proyecto-integrador-nine.vercel.app",  # URL de producción
+    "http://localhost:3000",          # Para seguir probando local
+]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],      # El asterisco permite CUALQUIER origen
+    allow_origins=origins,      # El asterisco permite CUALQUIER origen
     allow_credentials=False,  # DEBE ser False cuando usamos el asterisco "*"
     allow_methods=["*"],      # Permite POST, GET, etc.
     allow_headers=["*"],      # Permite cualquier encabezado
